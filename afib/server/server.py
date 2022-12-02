76493e60bd2fc1a98abe016d92ef4ff26eba87ef
tabeedteo <tab.eed.teo@gotoko.co.id>
2022-12-03 00:36:09 +0500

    valide json scheema

--- afib/server/server.py
+++ afib/server/server.py
@@ -5,6 +5,38 @@ from flask import jsonify, request
 from db.db import *
 from bson import json_util, ObjectId
 import json
+from marshmallow import Schema, fields, ValidationError
+
+
+class BaseSchema(Schema):
+    age = fields.Integer(required=True)
+    sex = fields.String(required=True)
+    height = fields.Float(required=True)
+    weight = fields.Float(required=True)
+    ritmi =  fields.Integer(required=True)
+    I = fields.Float(required=True)
+    II = fields.Float(required=True)
+    III = fields.Float(required=True)
+    aVF =  fields.Float(required=True)
+    aVR = fields.Float(required=True)
+    aVL = fields.Float(required=True)
+    V1 = fields.Float(required=True)
+    V2 = fields.Float(required=True)
+    V3 = fields.Float(required=True)
+    V4 =  fields.Float(required=True)
+    V5 =  fields.Float(required=True)
+    V6  =  fields.Float(required=True)
+    cp = fields.Integer(required=True)
+    trtbps = fields.Integer(required=True)
+    chol = fields.Integer(required=True)
+    fbs = fields.Integer(required=True)
+    restecg  = fields.Integer(required=True)
+    thalachh  = fields.Integer(required=True)
+    exng  = fields.Integer(required=True)
+    oldpeak =  fields.Float(required=True)
+    slp  = fields.Integer(required=True)
+    caa  = fields.Integer(required=True)
+    thall  = fields.Integer(required=True)
 
 class ApiIsAlive(Resource):
     def get(self):
@@ -14,8 +46,14 @@ class ApiIsAlive(Resource):
 class AtrialFibrillationApi(Resource):
     
     def post(self):
-        ecg_data = request.get_json()
-        print(ecg_data)
+        patient_data  = request.get_json()
+        schema = BaseSchema()
+        try:
+            result = schema.load(patient_data)
+        except ValidationError as err:
+             return jsonify(err.messages), 400
+        
+        return jsonify(result)
     
     def get(self):
         ecg = []
