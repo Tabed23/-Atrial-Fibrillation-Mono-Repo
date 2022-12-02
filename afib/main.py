9761dab4e2295cc6586b2c54a814945a49968432
tabeedteo <tab.eed.teo@gotoko.co.id>
2022-12-02 17:27:50 +0500

    make collection of ecg

--- afib/main.py
+++ afib/main.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, Response, jsonify
 from flask_restful import Api
 from db.db import *
-from server.server import ApiIsAlive
+from server.server import ApiIsAlive, AtrialFibrillationApi
 
 app = Flask(__name__)
 api = Api(app)
 
 
 api.add_resource(ApiIsAlive, '/api/v1/health')
-
+api.add_resource(AtrialFibrillationApi, '/api/v1/ecg_data')
 if __name__ == '__main__':
     InitDB()
     app.run(debug=True)
--- afib/server/server.py
+++ afib/server/server.py
@@ -2,6 +2,7 @@
  
 from flask_restful import Resource, Api
 from flask import jsonify
+from db.db import *
 
 class ApiIsAlive(Resource):
     def get(self):
@@ -12,4 +13,10 @@ class AtrialFibrillationApi(Resource):
     
     def post(self):
         pass
-        
+    
+    def get(self):
+        ecg = []
+        col = DB['ecg']
+        for e in col.find_one({}):
+            ecg.append(e)
+        return jsonify({'ecg' : ecg})
